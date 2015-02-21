4a91dae3532540e4cef16ec6e3273ea28c09b0da
Tongtao Charles Zhang <charlesztt@users.noreply.github.com>
2015-02-21 14:43:42 -0500

    Create init.el
    2015

---
+++ init.el
@@ -0,0 +1,37 @@
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+(getenv "PATH")
+ (setenv "PATH"
+(concat
+ "/usr/texbin" ":"
+(getenv "PATH")))
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(wakatime-api-key "")
+ '(wakatime-cli-path ""))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(global-wakatime-mode)
+(setq
+    backup-by-copying t
+    backup-directory-alist
+    '(("." . "~/.saves"))
+    delete-old-versions t
+    kept-new-versions 6 
+    kept-old-versions 2
+    version-control t)
