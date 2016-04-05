a5cf6ff157ec1ea5f87ea874e27e091249edc66d
Tongtao Charles Zhang <varanium@varanium.net>
2016-04-05 18:44:14 -0400

    recover company-jedi settings

--- general/init.el
+++ general/init.el
@@ -79,6 +79,12 @@
 ;; company
 (add-hook 'after-init-hook 'global-company-mode)
 
+;; company-jedi
+;; currently only available on Linux and Mac
+(defun my/python-mode-hook ()
+  (add-to-list 'company-backends 'company-jedi))
+(add-hook 'python-mode-hook 'my/python-mode-hook)
+
 ;; org-mode
 (add-hook 'org-mode-hook 'flyspell-buffer)
 (add-hook 'org-mode-hook 'turn-on-flyspell)
