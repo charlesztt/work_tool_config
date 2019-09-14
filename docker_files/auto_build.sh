b994d9747dacf32d45f39fca8b39b0513a81883d
Zhang <tongtao.zhang@siemens.com>
2019-09-14 10:44:18 -0400

    create tags

--- docker_files/auto_build.sh
+++ docker_files/auto_build.sh
@@ -1,9 +1,15 @@
 #!/user/bin/env bash
 cd docker_haruna_cpu
-docker build -t charlesztt/haruna_cpu ./
-cd ..
-cd docker_haruna_gpu
-docker build -t charlesztt/haruna_gpu ./
-cd ..
+docker build -t charlesztt/haruna_cpu:base ./
+cd ../docker_haruna_cpu_tensorflow
+docker build -t charlesztt/haruna_cpu:tensorflow ./
+cd ../docker_haruna_cpu_pytorch
+docker build -t charlesztt/haruna_cpu:pytorch ./
+docker tag charlesztt/haruna_cpu:pytorch charlesztt/haruna_cpu:torch
+docker tag charlesztt/haruna_cpu:pytorch charlesztt/haruna_cpu:latest
+# cd ..
+# cd docker_haruna_gpu
+# docker build -t charlesztt/haruna_gpu ./
+# cd ..
 docker push charlesztt/haruna_cpu
-docker push charlesztt/haruna_gpu
+# docker push charlesztt/haruna_gpu
