f3232af27feaeec2e17c715a0a6489828113a5dd
Tongtao Zhang <zhangtongtao@varanium.net>
2019-09-28 22:21:34 -0400

    docker files

--- docker_files/auto_build.sh
+++ docker_files/auto_build.sh
@@ -7,9 +7,23 @@ cd ../docker_haruna_cpu_pytorch
 docker build -t charlesztt/haruna_cpu:pytorch ./
 docker tag charlesztt/haruna_cpu:pytorch charlesztt/haruna_cpu:torch
 docker tag charlesztt/haruna_cpu:pytorch charlesztt/haruna_cpu:latest
-# cd ..
-# cd docker_haruna_gpu
-# docker build -t charlesztt/haruna_gpu ./
-# cd ..
+
+cd ../docker_haruna_gpu
+docker build -t charlesztt/haruna_gpu:base ./
+cd ../docker_haruna_gpu_tensorflow
+docker build -t charlesztt/haruna_gpu:tensorflow ./
+cd ../docker_haruna_gpu_pytorch
+docker build -t charlesztt/haruna_gpu:pytorch ./
+docker tag charlesztt/haruna_gpu:pytorch charlesztt/haruna_gpu:torch
+docker tag charlesztt/haruna_gpu:pytorch charlesztt/haruna_gpu:latest
+
+docker push charlesztt/haruna_cpu:base
+docker push charlesztt/haruna_cpu:tensorflow
+docker push charlesztt/haruna_cpu:pytorch
+docker push charlesztt/haruna_cpu:torch
 docker push charlesztt/haruna_cpu
-# docker push charlesztt/haruna_gpu
+docker push charlesztt/haruna_gpu:base
+docker push charlesztt/haruna_gpu:tensorflow
+docker push charlesztt/haruna_gpu:pytorch
+docker push charlesztt/haruna_gpu:torch
+docker push charlesztt/haruna_gpu
