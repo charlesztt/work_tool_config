c8804d0bbe34171ec44e9dd37610e30526d32a65
Tongtao Zhang <zhangtongtao@varanium.net>
2021-01-28 15:15:14 -0500

    update

---
+++ wow/class_selection/select_class_to_play.py
@@ -0,0 +1,15 @@
+import random
+
+classes = ['Warrior',
+           'Hunter',
+           'Mage',
+           'Warlock',
+           'Priest',
+           'Rougue',
+           'Paladin',
+           'Druid',
+           'Shaman',
+           'Monk',
+           'Demon Hunter',
+           'Death Knight',]
+print(random.choice(classes))
